3e0d75d2268d11b49a23c02dd977b46326819388
jyotidhikale28 <92510615+Jyoti28Dhikale@users.noreply.github.com>
2025-04-02 16:31:50 +0530

    Create Assignment 15.java

---
+++ Week 2/Assignment 15.java
@@ -0,0 +1,7 @@
+public class Modulus {
+    public static void main(String[] args) {
+        int dividend = 17, divisor = 5;
+        int remainder = dividend - (divisor * (dividend / divisor));
+        System.out.println("Modulus: " + remainder);
+    }
+}
